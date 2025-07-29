16d56adea4f584009eb509b84eb1fa1b60452d6c
“Kyle <klmrmt99@gmail.com>
2025-07-28 21:43:49 -0500

    Fixed object

--- src/auth/jwt/jwt.ts
+++ src/auth/jwt/jwt.ts
@@ -4,7 +4,7 @@ import { JwtPayload } from "../types";
 
 // Function to generate a JWT token for a given userId
 export const generateToken = (userID: string): string => {
-  const token = jwt.sign(userID, config.jwtSecret, {
+  const token = jwt.sign({ user_id: userID }, config.jwtSecret, {
     expiresIn: "1h",
   });
   return token;
